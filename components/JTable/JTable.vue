e153c403275a973e16d9a144d39650de446a7166
Konstantin Steinmiller <konstantin.steinmiller@factset.com>
2019-09-02 14:57:17 +0200

    header in JTable component

--- components/JTable/JTable.vue
+++ components/JTable/JTable.vue
@@ -1,10 +1,27 @@
 <template>
   <table>
+    <thead>
+      <tr>
+        <th
+            v-for="(label, index) in header"
+            :key="index"
+        >
+          {{ label }}
+        </th>
+      </tr>
+    </thead>
     <JTableRow
         v-for="(values, index) in items"
         :key="index"
         :values="values"
     />
+    <tfoot>
+      <tr>
+        <td>
+          some footer text
+        </td>
+      </tr>
+    </tfoot>
   </table>
 </template>
 
@@ -16,10 +33,22 @@ export default {
     items: {
       type: Array,
       required: true
-    }
+    },
+    header: {
+      type: Array
+    },
   },
   components: {
     JTableRow
   }
 }
 </script>
+
+<style>
+th {
+  text-align: left;
+  padding: 8px;
+  font-size: 13px;
+  color: #8d8d92;
+}
+</style>
